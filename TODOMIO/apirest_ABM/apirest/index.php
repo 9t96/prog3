66471cc606a8849a8fa8ed20ce280e3e8920e68b
laloo96 <ohmycaio@gmail.com>
2017-06-18 21:42:41 -0300

    algo

--- TODOMIO/apirest_ABM/apirest/index.php
+++ TODOMIO/apirest_ABM/apirest/index.php
@@ -2,9 +2,9 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-require '/vendor/autoload.php';
-require '/clases/AccesoDatos.php';
-require '/clases/cd.php';
+require './vendor/autoload.php';
+require './clases/AccesoDatos.php';
+require './clases/cd.php';
 
 $config['displayErrorDetails'] = true;
 $config['addContentLengthHeader'] = false;
--- TODOMIO/apirest_ABM/apirest/vendor/autoload.php
+++ TODOMIO/apirest_ABM/apirest/vendor/autoload.php
@@ -5,3 +5,5 @@
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
 return ComposerAutoloaderInit70e03fead106e5205edcbc39cd743b9f::getLoader();
+
+?>
